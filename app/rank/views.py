236eab773de74a27c3cf074a804a14f79c5a43c6
SongZihuan <jimsong2010@163.com>
2021-11-03 22:39:28 +0800

    feat: 调整闪现消息淡出的样式

--- app/rank/views.py
+++ app/rank/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, Flask
+from flask import render_template, Blueprint, Flask, request, abort
 from tool.type_ import Optional
 from app import views
 
@@ -6,16 +6,26 @@ rank = Blueprint("rank", __name__)
 app: Optional[Flask] = None
 
 
-@rank.route('/up/<int:page>')
-def rank_up(page: int):
-    data = views.website.get_rank(page, "DESC")
-    return render_template("rank/ranking.html", rank_info=data, ranking_name="高分榜")
+@rank.route('/up')
+def rank_up():
+    try:
+        page = int(request.args.get("page", 1))
+    except (ValueError, TypeError):
+        abort(404)
+    else:
+        data = views.website.get_rank(page, "DESC")
+        return render_template("rank/ranking.html", rank_info=data, ranking_name="高分榜")
 
 
-@rank.route('/down/<int:page>')
-def rank_down(page: int):
-    data = views.website.get_rank(page, "ASC")
-    return render_template("rank/ranking.html", rank_info=data, ranking_name="警示榜")
+@rank.route('/down')
+def rank_down():
+    try:
+        page = int(request.args.get("page", 1))
+    except (ValueError, TypeError):
+        abort(404)
+    else:
+        data = views.website.get_rank(page, "ASC")
+        return render_template("rank/ranking.html", rank_info=data, ranking_name="警示榜")
 
 
 def creat_ranking_website(app_: Flask):
